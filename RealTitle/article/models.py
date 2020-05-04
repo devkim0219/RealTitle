211ce24028b1813a9f9913c554cfcd769fe00acd
devkim0219 <devkim0219@email.mail>
2020-05-04 21:40:43 +0900

    테이블 수정(media_acc 컬럼 추가)

--- RealTitle/article/models.py
+++ RealTitle/article/models.py
@@ -18,7 +18,7 @@ class Result(models.Model):
     objects = models.Manager()
     result_id = models.CharField(primary_key=True, max_length=20)
     result_newtitle = models.CharField(max_length=200)
-    result_acc = models.FloatField()
+    result_acc = models.FloatField(null=True)
 
     def __str__(self):
         return (self.result_id)
@@ -28,6 +28,7 @@ class Media(models.Model):
     media_id = models.AutoField(primary_key=True)
     media_name = models.CharField(max_length=50)
     media_url = models.CharField(max_length=100)
+    media_acc = models.FloatField(null=True)
 
     def __str__(self):
         return (self.media_name)
